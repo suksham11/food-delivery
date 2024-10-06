6b6b14d1c96b419f1f95e1974cfe361b5f7a6531
suksham <129354626+suksham11@users.noreply.github.com>
2024-10-06 16:54:20 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,6 @@ import Cart from "./pages/Cart/Cart";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
 import Verify from "./pages/Verify/Verify";
-import MyOrders from "./pages/MyOrders/MyOrders";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -22,7 +21,6 @@ const App = () => {
           <Route path="/Cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
           <Route path="/verify" element={<Verify />} />
-          <Route path="/myorders" element={<MyOrders />} />
         </Routes>
       </div>
       <Footer />
